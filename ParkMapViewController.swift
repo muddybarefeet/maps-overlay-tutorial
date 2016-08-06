db2a7b91175da2c9172b84564696cb564a69652b
Anna Rogers <2muddybarefeet@gmail.com>
2016-08-06 10:23:05 -0700

    add zoom in to loction and change map view logic

--- ParkMapViewController.swift
+++ ParkMapViewController.swift
@@ -17,32 +17,47 @@ enum MapType: Int {
 
 class ParkMapViewController: UIViewController, MKMapViewDelegate {
   
-  @IBOutlet weak var mapTypeSegmentedControl: UISegmentedControl!
-  
-  @IBOutlet weak var map: MKMapView!
+    @IBOutlet weak var mapTypeSegmentedControl: UISegmentedControl!
     
-  var selectedOptions = [MapOptionsType]()
-  
-  override func viewDidLoad() {
-    super.viewDidLoad()
-  }
-  
-  func loadSelectedOptions() {
+    @IBOutlet weak var mapView: MKMapView!
+
+    var selectedOptions = [MapOptionsType]()
+    var park = Park(filename: "MagicMountain")
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let latDelta = park.overlayTopLeftCoordinate.latitude - park.overlayBottomRightCoordinate.latitude
+        // think of a span as a tv size, measure from one corner to another
+        let span = MKCoordinateSpanMake(fabs(latDelta), 0.0)
+        let region = MKCoordinateRegionMake(park.midCoordinate, span)
+        mapView.region = region
+    }
+    
+    func loadSelectedOptions() {
     // To be implemented
-  }
-  
-  override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+    }
+
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
     let optionsViewController = segue.destinationViewController as! MapOptionsViewController
     optionsViewController.selectedOptions = selectedOptions
-  }
-  
-  @IBAction func closeOptions(exitSegue: UIStoryboardSegue) {
+    }
+
+    @IBAction func closeOptions(exitSegue: UIStoryboardSegue) {
     let optionsViewController = exitSegue.sourceViewController as! MapOptionsViewController
     selectedOptions = optionsViewController.selectedOptions
     self.loadSelectedOptions()
-  }
-  
-  @IBAction func mapTypeChanged(sender: AnyObject) {
+    }
+
+    @IBAction func mapTypeChanged(sender: AnyObject) {
     // To be implemented
-  }
+        let mapType = MapType(rawValue: mapTypeSegmentedControl.selectedSegmentIndex)
+        switch (mapType!) {
+        case .Standard:
+            mapView.mapType = MKMapType.Standard
+        case .Hybrid:
+            mapView.mapType = MKMapType.Hybrid
+        case .Satellite:
+            mapView.mapType = MKMapType.Satellite
+        }
+    }
 }
